6063d35a0d22d057d65dc732943df1114be2c6d1
Ali Yanni <aliryanni@gmail.com>
2022-07-04 13:50:35 -0400

    updates API validations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,5 +1,5 @@
 const service = require("./reservations.service");
-const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function reservationExists(req, res, next) {
   const { reservationId } = req.params;
@@ -8,120 +8,87 @@ async function reservationExists(req, res, next) {
     res.locals.reservation = reservation;
     return next();
   }
-  next({
-    status: 404,
-    message: `Reservation ${reservationId} not found`,
-  })
+  next({ status: 404, message: `Reservation ${reservationId} not found` });
 }
 
 function hasData(req, res, next) {
-  if (req.body) {
+  if (req.body.data) {
     return next();
   }
-  next({
-    status: 400,
-    message: "Body must have data property."
-  })
-}   
+  next({ status: 400, message: "Body must have data property." });
+}
 
 function hasFirstName(req, res, next) {
-  const name = req.body.first_name;
+  const name = req.body.data.first_name;
   if (name) {
     return next();
   }
-  next({
-    status: 400,
-    message: "first_name property required.",
-  })
+  next({ status: 400, message: "first_name property required." });
 }
 
 function hasLastName(req, res, next) {
-  const name = req.body.last_name;
+  const name = req.body.data.last_name;
   if (name) {
     return next();
   }
-  next({
-    status: 400,
-    message: "last_name property required.",
-  })
+  next({ status: 400, message: "last_name property required." });
 }
 
 function hasMobileNumber(req, res, next) {
-  const phone = req.body.mobile_number;
+  const phone = req.body.data.mobile_number;
   if (phone) {
     return next();
   }
-  next({
-    status: 400,
-    message: "mobile_number property required.",
-  })
+  next({ status: 400, message: "mobile_number property required." });
 }
 
 function hasReservationDate(req, res, next) {
-  const date = req.body.reservation_date;
+  const date = req.body.data.reservation_date;
   if (date) {
     return next();
   }
-  next({
-    status: 400,
-    message: "reservation_date property required.",
-  })
+  next({ status: 400, message: "reservation_date property required." });
 }
 
 function validDate(req, res, next) {
-  const date = req.body.reservation_date;
+  const date = req.body.data.reservation_date;
   const valid = Date.parse(date);
-
   if (valid) {
     return next();
   }
-  next({
-    status: 400,
-    message: "reservation_date must be valid date.",
-  })
+  next({ status: 400, message: "reservation_date must be valid date." });
 }
 
 function hasReservationTime(req, res, next) {
-  const time = req.body.reservation_time;
-  if (time && typeof time === 'string') {
+  const time = req.body.data.reservation_time;
+  if (time && typeof time === "string") {
     return next();
   }
-  next({
-    status: 400,
-    message: "valid reservation_time property required.",
-  })
+  next({ status: 400, message: "Valid reservation_time property required." });
 }
 
 function validTime(req, res, next) {
   const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/;
-  const time = req.body.reservation_time;
+  const time = req.body.data.reservation_time;
   const valid = time.match(regex);
   if (valid) {
     return next();
   }
-  next({
-    status: 400,
-    message: "reservation_time must be valid time.",
-  })
+  next({ status: 400, message: "reservation_time must be valid time." });
 }
 
 function hasValidPeople(req, res, next) {
-  const people = req.body.people;
-  
-  if (people > 0) {
+  const people = req.body.data.people;
+  if (people > 0 && typeof people === "number") {
     return next();
   }
-  next({
-    status: 400,
-    message: "valid people property required"
-  })
+  next({ status: 400, message: "Valid people property required" });
 }
-
 
 async function list(req, res) {
   let date = req.query.date;
   let data = [];
-  if(date){
+  if (date) {
     data = await service.listByDate(date);
   }
   res.json({
@@ -129,9 +96,8 @@ async function list(req, res) {
   });
 }
 
-async function create(req, res){
-  const reservation = req.body;
-  console.log(reservation, "RESER");
+async function create(req, res) {
+  const reservation = req.body.data;
   const data = await service.create(reservation);
   res.status(201).json({ data });
 }
@@ -139,15 +105,15 @@ async function create(req, res){
 module.exports = {
   list: asyncErrorBoundary(list),
   create: [
-    hasData, 
-    hasFirstName, 
-    hasLastName, 
-    hasMobileNumber, 
+    hasData,
+    hasFirstName,
+    hasLastName,
+    hasMobileNumber,
     hasReservationDate,
     validDate,
     hasReservationTime,
     validTime,
     hasValidPeople,
-    asyncErrorBoundary(create)
-  ]
+    asyncErrorBoundary(create),
+  ],
 };
